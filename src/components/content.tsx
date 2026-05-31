249ffadf8e6c33cac401f6a77bdb388f75092580
zephyrty <1937176774@qq.com>
2026-05-31 22:32:24 +0800

    feat: render SelectionBar and FolderDrawer in MainContent

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,42 +1,32 @@
-import { Component, createMemo, createSignal, onMount } from 'solid-js';
+import { Component, createMemo } from 'solid-js';
 import { TopBar } from './top-bar';
 import { IndexSidebar } from './sidebar/index-sidebar';
 import { PackSidebar } from './sidebar/pack-sidebar';
 import { Nav } from './nav';
+import { SelectionBar } from './selection-bar';
+import { FolderDrawer } from './drawer/folder-drawer';
 import { useParams } from '@solidjs/router';
 import signalStore from '../utils/shared-signal';
 
 export const MainContent: Component<any> = (props) => {
   const params = useParams();
+  const isPackPage = createMemo(() => !!params.id);
 
-  const [showSearch, setShowSearch] = createSignal(true);
-
-  onMount(() => {
-    window.addEventListener('hashchange', (e) => {
-      signalStore.isManaging.set(false);
-      if (/\/pack\//.test(e.newURL)) {
-        setShowSearch(false);
-      } else {
-        setShowSearch(true);
-      }
-    });
-  });
-
-  const renderSidebar = createMemo(() => {
-    if (params.id) {
-      return <PackSidebar />;
-    } else {
-      return <IndexSidebar />;
-    }
-  });
   return (
-    <>
-      {renderSidebar()}
-      <div class="main-content">
-        <TopBar showSearch={showSearch()} />
-        {props.children}
+    <div class="flex h-full w-full">
+      <nav
+        class="flex flex-col items-center py-8 gap-6 z-10 w-12 shrink-0"
+        aria-label="Page navigation"
+      >
+        {isPackPage() ? <PackSidebar /> : <IndexSidebar />}
+      </nav>
+      <main class="flex-1 flex flex-col min-w-0 bg-stone-50">
+        <TopBar isPackPage={isPackPage()} />
+        <div class="flex-1 overflow-auto">{props.children}</div>
         <Nav total={signalStore.page()} />
-      </div>
-    </>
+      </main>
+      <SelectionBar />
+      <FolderDrawer />
+    </div>
   );
 };
